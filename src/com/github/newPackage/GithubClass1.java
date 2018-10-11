48f46df09e153ce80eed777404d1c51a6bb666b9
Harsha <teamindia#456>
2018-10-11 07:40:21 +0530

    [Harsha] Added Test methods to the Github Class1

--- src/com/github/newPackage/GithubClass1.java
+++ src/com/github/newPackage/GithubClass1.java
@@ -2,4 +2,20 @@ package com.github.newPackage;
 
 public class GithubClass1 {
 
+	public static void testMethod1() {
+		
+	}
+	
+public static void testMethod2() {
+		
+	}
+
+public static void testMethod3() {
+	
+}
+
+public static void testMethod4() {
+	
+}
+	
 }
